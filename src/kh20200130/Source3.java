5df0b9ac1c3242ad10a17e8bde7b0228907e0f69
Kim heonil <iamheonil@gmail.com>
2020-01-30 17:55:59 +0900

    KH UI/UX Engineering

--- src/kh20200130/Source3.java
+++ src/kh20200130/Source3.java
@@ -4,8 +4,20 @@ public class Source3 {
 
 	public static void main(String[] args) {
 		
-		// 참조형 데이터타입
+		// String(문자열) - char의 나열, 문장을 표현하는 데이터 타입.
+		
+		String fruit;			// String 변수 선언
+		fruit = "Apple";		// 변수에 값을 대입
+		System.out.println(fruit);			// 출력
 
+	
+		// 중복으로 선언이 가능
+		fruit = "Banana";
+		System.out.println(fruit);
+		
+		fruit = "Grape";
+		System.out.println("과일명 : " + fruit);
+		
 	}
 
 }
