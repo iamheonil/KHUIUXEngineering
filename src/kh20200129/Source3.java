b046d78a41059db7b99f71e16851cba39f91f999
Kim heonil <iamheonil@gmail.com>
2020-01-30 16:24:23 +0900

    0130

--- src/kh20200129/Source3.java
+++ src/kh20200129/Source3.java
@@ -7,11 +7,10 @@ public class Source3 {
 		// 변수의 선언과 동시에 초기화(Initialize)
 		int num = 123;
 		
-		int age;
-		
+//		int age;
 //		age = 25;
 				
-		System.out.println(age); // age = 25; 없이 출력하려고 한다면 쓰레기 값을 출력하려고 하기 때문에 아예 실행이 안된다.
+		System.out.println(num); // age = 25; 없이 출력하려고 한다면 쓰레기 값을 출력하려고 하기 때문에 아예 실행이 안된다.
 								// 변수를 초기화 해주고 사용해야함.
 								// 변수를 선언과 동시에 초기화 해야하는 경우가 있는데 다음에 그런 경우가 생긴다면 따로 설명해주신다고 함.
 
