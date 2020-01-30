2314cf2f14dc0fad24ae7e72aeae2ad2cf150bcc
Kim heonil <iamheonil@gmail.com>
2020-01-30 20:48:54 +0900

    KH UI/UX Engineering

---
+++ src/kh20200130/Source5.java
@@ -0,0 +1,16 @@
+package kh20200130;
+
+public class Source5 {
+	public static void main(String[] args) {
+
+		System.out.println("Ctrl + Alt + 아래 혹은 위로 방향키 -> 위, 혹은 아래로 밀어내며 복사");
+		System.out.println("Ctrl + D(Delete) -> 커서가 위치한 해당줄 삭제");
+		System.out.println("Alt + 방향키 -> 해당줄을 위 아래로 왔다갔다");
+		System.out.println("Ctrl + 방향키 -> 스크롤 / 근데 이거는 많이 안 쓸 거 같은데..");
+		
+	}
+}
+
+
+// 이항연산자 - 대입
+// = 
