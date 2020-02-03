077acbd6a2b8fa853dfe05f0ba64c25a52069ec3
Kim heonil <iamheonil@gmail.com>
2020-02-03 18:19:22 +0900

    KH UI/UX Engineering

---
+++ src/kh20200203/ScannerQuiz2.java
@@ -0,0 +1,38 @@
+package kh20200203;
+
+import java.util.Scanner;
+
+public class ScannerQuiz2 {
+	public static void main(String[] args) {
+
+// 학생 정보 관리 프로그램 제작.
+// 이름, 나이, 성별, 국어, 영어, 수학 6가지 정보를 저장할 수 있는 변수를 선언하고
+// 6가지 정보를 Scanner 를 이용해 입력 한 후 총점과 평균을 포함한 결과를 출력한다.
+	
+		Scanner sc = new Scanner(System.in);
+		
+		String name, gender;
+		int age, kor, eng, mat;
+		
+		System.out.print("이름을 입력하세요 : ");
+		name = sc.nextLine();
+		System.out.print("나이를 입력하세요 : ");
+		age = sc.nextInt();
+		sc.nextLine();
+		System.out.print("성별을 입력하세요 : ");
+		gender = sc.nextLine();
+		System.out.print("국어점수를 입력하세요 : ");
+		kor = sc.nextInt();
+		System.out.print("영어점수를 입력하세요 : ");
+		eng = sc.nextInt();
+		System.out.print("수학점수를 입력하세요 : ");
+		mat = sc.nextInt();
+		
+		int sum = kor + eng + mat;
+		double result = sum / (double)3;
+		
+		System.out.println("--- 결과 출력 ---");
+		System.out.println("이름\t 나이\t 성별\t 국어\t 영어\t 수학\t 총점\t 평균\t");
+		System.out.println(name + "\t" + age + "\t" + gender + "\t" + kor + "\t" + eng + "\t" + mat + "\t" + sum + "\t" + result);
+	}
+}
--- src/kh20200203/Source3.java
+++ src/kh20200203/Source3.java
@@ -1,8 +1,26 @@
 package kh20200203;
 
+import java.util.Scanner;
+
 public class Source3 {
 	public static void main(String[] args) {
 
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println(" -- 입력 -- ");
+		System.out.print("숫자를 입력 : ");
+		int num = sc.nextInt();
+		
+		System.out.print("문자를 입력 : ");
+		sc.nextLine(); // 버퍼의 남아있는 '\n'을 지워준다.
+//		위의 nextInt를 사용하게 되면 아래의 sc.nextLine() 이 먹통이 되는데 이것을 방지하고자 바로위의 sc.nextLine() 을 사용해준다.
+//		* 부가설명
+//		nextLine() 메소드를 사용하기 전에 nextLine() 이외의 모든 next() 계열 메소드를 사용한 적이 있다면 nextLine() 직전에 nextLine()을 한번 더 실행해야한다.
+		String str = sc.nextLine();
+		
+		System.out.println(" -- 출력 -- ");
+		System.out.println(num);
+		System.out.println(str);
 		
 	}
 }
