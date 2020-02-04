790d22bdfad56e0e393348a2b47c0ac5b75b2f5c
Kim heonil <iamheonil@gmail.com>
2020-02-04 21:34:54 +0900

    KH UI/UX Engineering

--- src/kh20200204/Loop_02.java
+++ src/kh20200204/Loop_02.java
@@ -95,14 +95,12 @@ public class Loop_02 {
 //		}
 
 		// Q6.
-//		for (int i = 1; i <= 5; i++) {
-//			System.out.println(i);
-//			for (int j = 1; j <= 5; j++) {
-//				j = i;
-//				System.out.print(j);
-//				
-//			}
-//		}
+		for (int i = 1; i <= 6; i++) {
+			System.out.print(i);
+			i++;
+		}for (int j = 1; j <= 5; j++) {
+		System.out.println(j);		
+	}
 		
 //		// Q8.
 //		for (int i = 1; i < 6; i++) {
