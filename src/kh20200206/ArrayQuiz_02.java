cfe7c6ee9825b302fb527ffcdffeb684f4bef8c5
Kim heonil <iamheonil@gmail.com>
2020-02-06 21:47:00 +0900

    KH UI/UX Engineering

---
+++ src/kh20200206/ArrayQuiz_02.java
@@ -0,0 +1,41 @@
+package kh20200206;
+
+public class ArrayQuiz_02 {
+	public static void main(String[] args) {
+
+		int arr[] = { 10, 17, 3, 9, 27, 10, 8, 9, 13, 21 };
+		int invert_arr[] = new int[10];
+		
+		int first = 0;
+		int sec = 0;
+		
+//		for (int i = 0; i <= 9; i++) {
+//				invert_arr[i] = arr[9-i];
+//				System.out.print(invert_arr[i] + ", ");
+//			}
+		
+		
+//		for (int i = 0; i < arr.length; i++) {
+//			if (i % 2 == 0) {
+//				sec += arr[i];
+//				
+//			} else if(arr[i] % 2 != 0) {
+//				first += arr[i];
+//			}
+//		} System.out.println(first - sec);
+		
+		
+		int rank_arr[] = new int[10];
+		for (int i = 0; i < arr.length; i++) {
+//			System.out.print(arr[i] + ", ");
+			
+//			rank_arr[i] += 
+		}
+		
+//		System.out.println();
+//		System.out.print(rank_arr[0]);
+
+		
+	
+	}
+}
