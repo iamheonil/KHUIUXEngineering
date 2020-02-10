db8087b8fe435ac96cbf2a3b1946aefd5df6e05d
Kim heonil <iamheonil@gmail.com>
2020-02-10 17:54:26 +0900

    KH UI/UX Engineering

--- src/kh20200210/Array2DQuiz.java
+++ src/kh20200210/Array2DQuiz.java
@@ -3,8 +3,8 @@ package kh20200210;
 public class Array2DQuiz {
 	public static void main(String[] args) {
 
-		int[][] QArr = new int[5][5];
-		int count = 0;
+//		int[][] QArr = new int[5][5];
+//		int count = 0;
 
 		// Q1 .
 //		for (int i = 0; i < QArr.length; i++) {
@@ -46,10 +46,27 @@ public class Array2DQuiz {
 //		15   24   25   20   7
 //		14   23   22   21   8
 //		13   12   11   10   9
+//		
+//		01	02	03	04	05
+//		11	12	13	14	15
+//		21	22	23	24	25
+//		31	32	33	34	35
+//		41	42	43	44	45
+		
+		// j ->
+		// i ↓
+		
+		// j 1~5까지 증가, i 1~5까지 증가, j 5~1까지 감소, i 4~1까지 감소, j 1부터 4까지, i 1부터 3까지,
+		
+		int[][] QArr = new int[10][10];
+		int count = 0;
+		
 		for (int i = 0; i < QArr.length; i++) {
 			for (int j = 0; j < QArr[i].length; j++) {
-
+				
 				QArr[i][j] = 1 + count++;
+				
+			
 
 			}
 		}
