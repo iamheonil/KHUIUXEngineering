8e6e3173d3b8e72ec97eb2640dde63dcc110930e
Kim heonil <iamheonil@gmail.com>
2020-02-16 23:42:18 +0900

    0216 Practice

---
+++ src/kh20200216/Main.java
@@ -0,0 +1,11 @@
+package kh20200216;
+
+public class Main {
+	public static void main(String[] args) {
+
+		Student stu = new Student("±Ë«Â¿œ", 26, 80, 180, "20200216", 3, 4.5);
+		
+		stu.show();
+		
+	}
+}
