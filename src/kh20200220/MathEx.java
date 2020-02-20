330b826ec8d8e59e1d41fa6cccd3840b968ff9ee
Kim heonil <iamheonil@gmail.com>
2020-02-20 18:15:58 +0900

    KH UI/UX Engineering

---
+++ src/kh20200220/MathEx.java
@@ -0,0 +1,27 @@
+package kh20200220;
+
+public class MathEx {
+	public static void main(String[] args) {
+		
+		// 원주율 출력
+		System.out.println(Math.PI);
+		
+		// -10 의 절대값
+		System.out.println(Math.abs(-10));
+		
+		// 3.7의 올림값
+		System.out.println(Math.ceil(3.7));
+		
+		// 3.7의 내림값
+		System.out.println(Math.floor(3.7));
+		
+		// -3.7의 반올림값
+		System.out.println(Math.round(-3.7));		
+		
+		// 2.1의 3제곱값
+		System.out.printf("%.3f", Math.pow(2.1, 3));
+		
+		// 랜덤
+//		System.out.println(Math.random() * 2);
+	}
+}
