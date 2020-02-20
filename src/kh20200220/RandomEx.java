d274e1ac47d7d1db3ce34aa1e976b48539796081
Kim heonil <iamheonil@gmail.com>
2020-02-20 19:44:25 +0900

    KH UI/UX Engineering

---
+++ src/kh20200220/RandomEx.java
@@ -0,0 +1,24 @@
+package kh20200220;
+
+import java.util.Random;
+
+public class RandomEx {
+	public static void main(String[] args) {
+
+		Random ran = new Random();
+		
+		System.out.println(ran.nextInt(100));
+		
+		System.out.println();
+		
+		System.out.println(ran.nextBoolean());
+		
+		System.out.println();
+		
+		System.out.println(ran.nextDouble());
+		
+		System.out.println();
+		
+		
+	}
+}
