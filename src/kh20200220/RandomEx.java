0c9c1f2832ee5e4482e5362802417457401fc602
Kim heonil <iamheonil@gmail.com>
2020-02-20 21:28:36 +0900

    KH UI/UX Engineering

--- src/kh20200220/RandomEx.java
+++ src/kh20200220/RandomEx.java
@@ -1,5 +1,6 @@
 package kh20200220;
 
+import java.util.Arrays;
 import java.util.Random;
 
 public class RandomEx {
@@ -19,6 +20,24 @@ public class RandomEx {
 		
 		System.out.println();
 		
+		System.out.println(ran.nextInt(3));
+		System.out.println(ran.nextInt(3) + 1);
 		
+		
+		int[] arr = new int[6];
+		
+		for(int i = 0; i < arr.length; i++) {
+			arr[i] = ran.nextInt(45) + 1;
+			for (int j = 0; j < i; j++) {
+				if(arr[i] == arr[j]) {
+					i--;
+					break;
+				}
+			}
+		}	
+		
+		Arrays.sort(arr);							// 정렬 
+		System.out.println(Arrays.toString(arr)); 	// 출력
+			
 	}
 }
---
+++ src/kh20200220/RandomQuiz2.java
@@ -0,0 +1,84 @@
+package kh20200220;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class RandomQuiz2 {
+	public static void main(String[] args) {
+
+		int user;
+		int com;
+		int count = 0;
+
+		Random ran = new Random();
+		Scanner sc = new Scanner(System.in);
+
+		do {
+			System.out.print("몇 번 부를까요? : ");
+			user = sc.nextInt();
+			
+			if (user == 1) {
+				count++;
+			} else if (user == 2) {
+				count++;
+				count++;
+			} else if (user == 3) {
+				count++;
+				count++;
+				count++;				
+			} else {
+				System.out.println("부정행위");
+				return;
+			}
+		} while (count < 31);
+		System.out.println("종료!");
+		
+
+
+	} // Method End
+} // Class End
+
+
+//do {
+//System.out.print("한번 두번 세번 부를 값을 입력하세요. : ");
+//user = sc.nextInt();
+//if (user == 1) {
+//	
+//	count++;
+//	System.out.println(count);
+//} else if (user == 2) {
+//	System.out.println("유저 : 2!");
+//	count++;
+//	count++;
+//} else if (user == 3) {
+//	System.out.println("유저 : 3!");
+//	count++;
+//	count++;
+//	count++;
+//} else {
+//	System.out.println("바른 값을 입력하지 않으셨기 때문에 패배입니다.\n");
+//	break;
+//}
+//
+//System.out.println(count);
+//com = ran.nextInt(3) + 1;
+//
+//if (com == 1) {
+//	System.out.println("콤푸타 : 1!");
+//	count++;
+//
+//} else if (com == 2) {
+//	System.out.println("콤푸타 : 2!");
+//	count++;
+//	count++;
+//} else if (com == 3) {
+//	System.out.println("콤푸타 : 3!");
+//	count++;
+//	count++;
+//	count++;
+//}
+//
+////System.out.println("현재 숫자 : " + count);
+//
+//} while (count < 31);
+//System.out.println("게임이 끝났습니다.");
