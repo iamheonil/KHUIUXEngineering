bf78d6c4db501ff17c8539da30a310019087f5c7
Kim heonil <iamheonil@gmail.com>
2020-02-21 18:18:18 +0900

    KH UI/UX Engineering

--- src/kh20200221_wrapper/WrapperEx.java
+++ src/kh20200221_wrapper/WrapperEx.java
@@ -3,6 +3,26 @@ package kh20200221_wrapper;
 public class WrapperEx {
 	public static void main(String[] args) {
 
+		// Wrapper : 포장지
+
+		// 기본데이터타입 -> 객체화
+		
+		int i1 = 10;
+		
+		Integer iVal1 = new Integer(100);
+
+		System.out.println(i1);
+		System.out.println(iVal1);
+
+		Byte bVal = new Byte((byte)1);
+		Short sVal; 
+		Long lVal;
+		Float fVal;
+		double dVal;
+		Character cVal;
+		boolean boolVal; 
+		
+		System.out.println(bVal);
 		
 		
 	}
