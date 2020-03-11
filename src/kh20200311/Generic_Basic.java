fb5682c5fcc3385852c9dafaeb34fb4bdcfa13ff
Kim heonil <iamheonil@gmail.com>
2020-03-11 18:16:34 +0900

    KH UI/UX Engineering

---
+++ src/kh20200311/Generic_Basic.java
@@ -0,0 +1,38 @@
+package kh20200311;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Generic_Basic {
+	public static void main(String[] args) {
+
+		// 타입을 지정하지 않고 구현한다면 Object로 사용된다.
+		List list = new ArrayList();
+		list.add("Hello");
+		
+		// 부모인 List<String> 과 자식인 ArrayList 의 자료형이 다르다면 에러가 발생한다.
+		// 무조건 같게 써줘야 한다b
+		//  <E> 를 <String> 으로 지정했다.
+		List<String> sList = new ArrayList<>();
+
+		List<Integer> iList = new ArrayList<>();
+		// List<Int> iList = new ArrayList<>(); 는 사용 불가
+		// Wrapper 클래스를 사용해야 한다.
+		
+		// 타입 안정성
+		// 제네릭을 사용할 때 자료형을 꼭 명시해줘야한다.
+		sList.add("Hi");
+		
+		// String str1 = list.get(0); -> 타입 안정성이 없기에 형변환이 필요하다.
+		String str1 = (String) list.get(0);
+		
+		// String으로 처리되어 형변환이 필요없다.
+		String str2 = sList.get(0);
+		
+		System.out.println(str1);
+		System.out.println(str2);
+		
+		
+		
+	}
+}
---
+++ src/kh20200311/Generic_Multi.java
@@ -0,0 +1,56 @@
+package kh20200311;
+
+// 타입 파라미터를 여러개 사용하는 방법.
+
+// 멀티 제네릭 클래스
+class Class04<T1, T2> {
+	
+	private T1 data1;
+	private T2 data2;
+	
+
+// 멀티 제네릭 메소드
+	public <K1, K2> K2 test(K1 data) {
+		K2 result = (K2) data;
+		
+		return result;
+	}
+
+
+	public T1 getData1() {
+		return data1;
+	}
+
+
+	public void setData1(T1 data1) {
+		this.data1 = data1;
+	}
+
+
+	public T2 getData2() {
+		return data2;
+	}
+
+
+	public void setData2(T2 data2) {
+		this.data2 = data2;
+	}
+	
+	
+}
+
+public class Generic_Multi {
+	public static void main(String[] args) {
+
+		Class04<Integer, String> c04 = new Class04<>();
+		
+		c04.setData1(123);
+		c04.setData2("ABC");
+		
+		
+		// 위의 Integer, String 선언을 한 상태여도
+		// 새로 생성자를 만들어서 선언해줘도 사용하는데 무관하다.
+		
+		c04.<Boolean, Double>test(true);
+	}
+}
