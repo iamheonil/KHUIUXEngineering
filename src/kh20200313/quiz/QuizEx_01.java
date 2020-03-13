0ba88d977c756d1169a91dd52e3bcba2c05a6e8d
Kim heonil <iamheonil@gmail.com>
2020-03-13 21:44:04 +0900

    KH UI/UX Engineering

--- src/kh20200313/quiz/QuizEx_01.java
+++ src/kh20200313/quiz/QuizEx_01.java
@@ -1,13 +1,65 @@
 package kh20200313.quiz;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class QuizEx_01 {
 	public static void main(String[] args) {
 
-		File file = new File("./src/kh20200313/quiz/Snippet.java", "Source.txt");
+		// 원본
+		File sTxt = new File("./src/kh20200313/quiz/", "Source.txt");
+		// 복사본
+		File dTxt = new File("./src/kh20200313/quiz/", "Dest.txt");
 
-		System.out.println(file.exists());
+//		System.out.println(sTxt.exists());
+//		System.out.println(dTxt.exists());
 
+		FileInputStream fis = null;
+		FileOutputStream fos = null;
+
+		byte[] buf = new byte[1024];
+		int len = -1;
+		int total = 0;
+		int in = 0;
+		StringBuilder sb = new StringBuilder();
+
+		try {
+//			fos = new FileOutputStream(sTxt);
+			fis = new FileInputStream(sTxt);
+			while ((len = fis.read(buf)) != -1) {
+				sb.append(new String(buf, 0, len));
+
+				total += len;
+			}
+
+			len = fis.read(buf);
+
+//			fos.write(buf, 0, len);
+//			fos.flush();
+			System.out.println(total);
+			fos = new FileOutputStream(dTxt, true);
+			while ((in = fis.read(buf)) != -1) {
+				fos.write(buf, 0, in);
+			}
+			fos.flush();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				if (fos != null) {
+					fos.close();
+				}
+				if (fis != null) {
+					fis.close();
+				}
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		System.out.println("복사 완료! 총 : " + total + " Byte");
 	}
 }
