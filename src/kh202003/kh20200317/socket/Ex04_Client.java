5726a44f92af08cc6587077a9a52f8b4b20497cc
Kim Heonil <iamheonil@gmail.com>
2020-03-18 02:15:43 +0900

    KH UI/UX Engineering

--- src/kh202003/kh20200317/socket/Ex04_Client.java
+++ src/kh202003/kh20200317/socket/Ex04_Client.java
@@ -1,9 +1,15 @@
 package kh202003.kh20200317.socket;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
 
 public class Ex04_Client {
 	public static void main(String[] args) {
@@ -12,11 +18,54 @@ public class Ex04_Client {
 //		 - out : PrintWriter
 //		 - file : File
 //		 - in : FileReader
-		
+
 		Socket sock = null;
+		FileReader in = null;
 		PrintWriter out = null;
 		File file = null;
-		FileReader in = null;
-		
+		Scanner sc = new Scanner(System.in);
+		BufferedReader br = null;
+		char[] cbuf = new char[1024];
+		int len = -1;
+
+		try {
+//			sock = new Socket("192.168.20.25", 10009);
+			sock = new Socket("192.168.0.2", 10009);
+
+			file = new File("./src/kh202003/kh20200317/socket/result/copy.txt");
+			System.out.print(file + "을 불러왔습니다.");
+			System.out.println();
+			
+
+			in = new FileReader(file);
+			
+			out = new PrintWriter(sock.getOutputStream(), true);
+			System.out.println(file.length());
+			
+			while ((len = in.read(cbuf)) != -1) {
+				out.write(cbuf, 0, len);
+				out.flush();
+			}
+
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+
+			try {
+				if (sock != null) {
+					sock.close();
+				}
+				if (in != null) {
+					in.close();
+				}
+				if (out != null) {
+					out.close();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 }
--- src/kh202003/kh20200317/socket/Ex04_Server.java
+++ src/kh202003/kh20200317/socket/Ex04_Server.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -22,22 +23,52 @@ public class Ex04_Server {
 		BufferedReader in = null;
 		File file = null;
 		FileWriter out = null;
+		char[] cbuf = new char[1024];
+		int len = -1;
+		File checkF = new File("./src/kh202003/kh20200317/socket/result/Server.txt");
 
+		String src = "./src/kh202003/kh20200317/socket/result/";
+		
 		try {
 			serv = new ServerSocket(10009);
-			sock = new Socket("192.168.20.25", 10009);
-			
+//			sock = new Socket("192.168.20.25", 10009);
+//			sock = new Socket("192.168.0.2", 10009);
 			System.out.println("쏘켓 생성 완료!");
+
+			sock = serv.accept();
 			InetAddress ip = sock.getInetAddress();
 			int port = sock.getPort();
-			System.out.println("아이피는 : "+ ip + " 포트는 : " + port);
+			System.out.println("아이피는 : " + ip + " 포트는 : " + port);
+			System.out.println("사람이 들어와따 !\n" + ip.getHostAddress() + " : " + sock.getPort());
+
+			in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+
+			out = new FileWriter(new File(src + "Server.txt"), false);
+
+			while((len = in.read(cbuf)) != -1) {
+				out.write(cbuf, 0, len);
+			}
 			
+			out.flush();
 			
-			
-			
-			
+			System.out.println("복사 완료");
+			System.out.println(checkF.length());
 		} catch (IOException e) {
 			e.printStackTrace();
+		} finally {
+			try {
+				if (sock != null) {
+					sock.close();
+				}
+				if (serv != null) {
+					serv.close();
+				}
+				if (in != null) {
+					in.close();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
 
 	}
